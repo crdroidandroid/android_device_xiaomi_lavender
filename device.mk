dd837a8138680e59cd9d34d163022b9f6af61711
Erfan Abdi <erfangplus@gmail.com>
2021-09-10 20:54:14 +0200

    lavender: Prepare to move to in-tree HALs
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# QCOM namepsaces
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
