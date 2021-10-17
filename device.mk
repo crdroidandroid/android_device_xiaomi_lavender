f18a42aa590929b458889ffdf7f50efba1418d12
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-10-17 17:45:15 +0530

    lavender: switch to prebuilt rmnetctl and libtinyxml
    * Taken from lavender-user-10-QKQ1.190910.002-V12.5.1.0.QFGMIXM-release-keys
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    Signed-off-by: xenxynon <flynryder427@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -172,8 +172,6 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libqdMetaData \
     libqdMetaData.vendor \
-    libtinyxml \
-    libtinyxml.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
@@ -380,7 +378,6 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
-    librmnetctl \
     libprotobuf-cpp-full \
     rild
 
