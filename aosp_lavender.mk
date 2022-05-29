51286c1aeffc749fe5930d6b4d5aa76f8f9e92ff
ImPrashantt <prashant33968@gmail.com>
2022-05-29 02:45:36 +0000

    lavender: Update FP from `raven` (May)
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -35,4 +35,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="lavender" \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT :="xiaomi/lavender/lavender:9/QKQ1.190910.002/V11.0.1.0.QFGMIXM:user/release-keys"
+BUILD_FINGERPRINT :="google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
